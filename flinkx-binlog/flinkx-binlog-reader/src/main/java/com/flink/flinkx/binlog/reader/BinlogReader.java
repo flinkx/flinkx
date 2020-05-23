23e7a4e0ebf2566e73b02712ed6f874349f48baf
gerry <blog@gerrywen.com>
2020-05-23 11:32:09 +0800

    增加binlog读写

---
+++ flinkx-binlog/flinkx-binlog-reader/src/main/java/com/flink/flinkx/binlog/reader/BinlogReader.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flink.flinkx.binlog.reader;
+
+import com.flink.flinkx.config.DataTransferConfig;
+import com.flink.flinkx.config.ReaderConfig;
+import com.flink.flinkx.reader.BaseDataReader;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.types.Row;
+
+/**
+ * @company: www.dtstack.com
+ * @author: toutian
+ * @create: 2019/7/4
+ */
+public class BinlogReader extends BaseDataReader {
+
+    private BinlogConfig binlogConfig;
+
+    @SuppressWarnings("unchecked")
+    public BinlogReader(DataTransferConfig config, StreamExecutionEnvironment env) {
+        super(config, env);
+        ReaderConfig readerConfig = config.getJob().getContent().get(0).getReader();
+
+        try {
+            binlogConfig = objectMapper.readValue(objectMapper.writeValueAsString(readerConfig.getParameter().getAll()), BinlogConfig.class);
+        } catch (Exception e) {
+            throw new RuntimeException("解析binlog Config配置出错:", e);
+        }
+    }
+
+    @Override
+    public DataStream<Row> readData() {
+        BinlogInputFormat format = new BinlogInputFormat();
+        format.setBinlogConfig(binlogConfig);
+        format.setRestoreConfig(restoreConfig);
+        format.setLogConfig(logConfig);
+        format.setTestConfig(testConfig);
+        return createInput(format);
+    }
+
+}
